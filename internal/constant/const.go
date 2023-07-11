efc88c4b248ad63f569c02c8e223eb25b17db729
dawnzzz <635441876@qq.com>
2023-07-11 22:36:50 +0800

    add constant

---
+++ internal/constant/const.go
@@ -0,0 +1,15 @@
+package constant
+
+const (
+	AuthHeader        = "Authentication"
+	AuthPrefix        = "Bearer"
+	TokenLength       = 16
+	CommitLength      = 64
+	UserIDKey         = "user_id"
+	DefaultBranch     = "main"
+	FileSavaDir       = "blobs"
+	MaxUserName       = 200
+	MaxDraftLength    = 20
+	MaxTagLength      = 20
+	MaxRepositoryName = 200
+)
