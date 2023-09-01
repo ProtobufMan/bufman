2c833f167c0232d48d1c0ba9bd505361aa1e9511
dawnzzz <635441876@qq.com>
2023-09-01 12:03:21 +0800

    update bufman config

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"github.com/ProtobufMan/bufman/internal/constant"
 	"github.com/docker/docker/client"
+	"github.com/gin-gonic/gin"
 	"github.com/olivere/elastic/v7"
+	"github.com/spf13/viper"
 	"gorm.io/gorm"
 	"os"
 	"time"
@@ -14,45 +16,65 @@ import (
 TODO ！！！这是临时的，之后改为读取配置文件
 */
 const (
-	mysqlDSNKey   = "IDL_MGR_MYSQL_DSN"
-	serverHostKey = "IDL_MGR_SERVER_HOST"
+	mysqlDSNKey        = "BUFMAN_MYSQL_DSN"
+	pageTokenSecretKey = "BUFMAN_PAGE_TOKEN_SECRET"
+	esUsernameKey      = "BUFMAN_ES_USERNAME"
+	esPasswordKey      = "BUFMAN_ES_PASSWORD"
+)
+
+const (
+	configFileName = "config.yaml"
+	configFileType = "yaml"
 )
 
 type Config struct {
-	BufMan        BufMan
-	Docker        Docker
-	ElasticSearch ElasticSearch
+	BufMan        BufMan        `mapstructure:"bufman"`
+	MySQL         MySQL         `mapstructure:"mysql"`
+	Docker        Docker        `mapstructure:"docker"`
+	ElasticSearch ElasticSearch `mapstructure:"elastic_search"`
 }
 
 type BufMan struct {
-	ServerHost string
-	MysqlDsn   string
+	Mode       string `mapstructure:"mode"`
+	ServerHost string `mapstructure:"server_host"`
 
-	PageTokenExpireTime time.Duration
-	PageTokenSecret     string
+	PageTokenExpireTime time.Duration `mapstructure:"page_token_expire_time"`
+	PageTokenSecret     string        `mapstructure:"page_token_secret"`
+
+	UseFSStorage bool `mapstructure:"use_fs_storage"`
+}
+
+type MySQL struct {
+	MysqlDsn           string        `mapstructure:"mysql_dsn"`
+	MaxOpenConnections int           `mapstructure:"max_open_connections"`
+	MaxIdleConnections int           `mapstructure:"max_idle_connections"`
+	MaxLifeTime        time.Duration `mapstructure:"max_life_time"`
+	MaxIdleTime        time.Duration `mapstructure:"max_idle_time"`
 }
 
 type Docker struct {
-	Host       string
-	CACertPath string
-	CertPath   string
-	KeyPath    string
+	Host       string `mapstructure:"host"`
+	CACertPath string `mapstructure:"ca_cert_path"`
+	CertPath   string `mapstructure:"cert_path"`
+	KeyPath    string `mapstructure:"key_path"`
 }
 
 type ElasticSearch struct {
-	Urls     []string
-	Username string
-	Password string
+	Urls     []string `mapstructure:"urls"`
+	Username string   `mapstructure:"username"`
+	Password string   `mapstructure:"password"`
 }
 
 var (
 	DataBase   *gorm.DB
-	Properties *Config
+	Properties = &Config{}
 )
 
 func LoadConfig() {
+	// 默认值
 	Properties = &Config{
 		BufMan: BufMan{
+			Mode:       gin.DebugMode,
 			ServerHost: "bufman.io",
 
 			PageTokenExpireTime: time.Minute * 10, // 默认过期时间为10分钟
@@ -71,23 +93,60 @@ func LoadConfig() {
 		},
 	}
 
-	if mysqlDSNENV := os.Getenv(mysqlDSNKey); mysqlDSNENV != "" {
-		Properties.BufMan.MysqlDsn = mysqlDSNENV
-	}
-	if serverHostENV := os.Getenv(serverHostKey); serverHostENV != "" {
-		Properties.BufMan.ServerHost = serverHostENV
-	}
+	// 从配置文件中读取
+	loadFromFile()
 
-	if err := os.MkdirAll(constant.FileSavaDir, 0666); err != nil {
-		panic(err)
+	// 从环境变量中读取
+	loadFromENV()
+
+	gin.SetMode(Properties.BufMan.Mode)
+
+	if Properties.BufMan.UseFSStorage {
+		if err := os.MkdirAll(constant.FileSavaDir, 0666); err != nil {
+			panic(err)
+		}
 	}
 
 	// test docker config
-	cli, err := NewDockerClient()
+	dockerCli, err := NewDockerClient()
 	if err != nil {
 		panic(err)
 	}
-	defer cli.Close()
+	defer dockerCli.Close()
+
+	// test es config
+	_, err = NewEsClient()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadFromFile() {
+	viper.SetConfigFile(configFileName)
+	viper.SetConfigType(configFileType)
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	if err := viper.Unmarshal(&Properties); err != nil {
+		panic(err)
+	}
+}
+
+func loadFromENV() {
+	if mysqlDSNENV := os.Getenv(mysqlDSNKey); mysqlDSNENV != "" {
+		Properties.MySQL.MysqlDsn = mysqlDSNENV
+	}
+	if pageTokenSecretENV := os.Getenv(pageTokenSecretKey); pageTokenSecretENV != "" {
+		Properties.BufMan.PageTokenSecret = pageTokenSecretENV
+	}
+	if esUsernameENV := os.Getenv(esUsernameKey); esUsernameENV != "" {
+		Properties.ElasticSearch.Username = esUsernameENV
+	}
+	if esPasswordENV := os.Getenv(esPasswordKey); esPasswordENV != "" {
+		Properties.ElasticSearch.Password = esPasswordENV
+	}
 }
 
 func NewDockerClient() (*client.Client, error) {
@@ -106,8 +165,8 @@ func NewDockerClient() (*client.Client, error) {
 }
 
 func NewEsClient() (*elastic.Client, error) {
-	if len(Properties.ElasticSearch.Urls) == 0 {
-		return nil, errors.New("no elastic search config")
+	if Properties.BufMan.UseFSStorage {
+		return nil, errors.New("use fs as storage")
 	}
 
 	c, err := elastic.NewClient(elastic.SetURL(Properties.ElasticSearch.Urls...), elastic.SetBasicAuth(Properties.ElasticSearch.Username, Properties.ElasticSearch.Password))
--- internal/model/init.go
+++ internal/model/init.go
@@ -7,20 +7,21 @@ import (
 )
 
 func InitDB() {
-	dsn := config.Properties.BufMan.MysqlDsn
-	var db *gorm.DB
+	dsn := config.Properties.MySQL.MysqlDsn
+	var DB *gorm.DB
 	var err error
 	if dsn == "" {
 		panic("MySQL DSN is empty")
 	} else {
-		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{TranslateError: true})
+		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{TranslateError: true})
 	}
+
 	if err != nil {
 		panic(err)
 	} else {
-		config.DataBase = db
+		config.DataBase = DB
 		// init table
-		initErr := db.AutoMigrate(
+		initErr := DB.AutoMigrate(
 			&Repository{},
 			&Commit{},
 			&Tag{},
@@ -35,4 +36,14 @@ func InitDB() {
 			panic(initErr)
 		}
 	}
+
+	db, err := config.DataBase.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxOpenConns(config.Properties.MySQL.MaxOpenConnections)
+	db.SetMaxIdleConns(config.Properties.MySQL.MaxIdleConnections)
+	db.SetConnMaxLifetime(config.Properties.MySQL.MaxLifeTime)
+	db.SetConnMaxIdleTime(config.Properties.MySQL.MaxIdleTime)
 }
